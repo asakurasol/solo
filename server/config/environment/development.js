bbd7ece34d45b75117db208cdab54d10356f82e6
asakurasol <mamamia1734@gmail.com>
2014-12-01 12:05:54 -0800

    scaffolded application

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/201410solo-dev'
+  },
+
+  seedDB: true
+};
